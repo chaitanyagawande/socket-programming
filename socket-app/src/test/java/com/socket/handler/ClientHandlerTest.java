c8ac3175531f800837b9115f1abb9c3fc60dec96
chaitanyagawande <cgawande12@gmail.com>
2024-02-18 18:26:01 -0800

    Added code for performance testing.

---
+++ socket-app/src/test/java/com/socket/handler/ClientHandlerTest.java
@@ -0,0 +1,60 @@
+package com.socket.handler;
+
+import static org.mockito.Mockito.*;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Base64;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.socket.utils.SessionManager;
+
+public class ClientHandlerTest {
+
+    @Mock
+    private Socket mockSocket;
+    @Mock
+    private SessionManager mockSessionManager;
+
+    private ClientHandler clientHandler;
+
+    @SuppressWarnings("deprecation")
+    @BeforeEach
+    void setUp() throws IOException {
+        MockitoAnnotations.initMocks(this);
+
+        String simulatedInput = Base64.getEncoder().encodeToString("testUser\nquit".getBytes());
+        ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(simulatedInput.getBytes());
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(byteArrayInputStream);
+
+        when(mockSocket.getInputStream()).thenReturn(bufferedInputStream);
+
+        clientHandler = new ClientHandler(mockSocket, mockSessionManager);
+    }
+
+    @Test
+    void testClientHandlerReceivesMessagesAndClosesConnection() throws IOException {
+        Thread clientHandlerThread = new Thread(clientHandler);
+        clientHandlerThread.start();
+
+
+        try {
+            clientHandlerThread.join();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("Test interrupted", e);
+        }
+
+
+        verify(mockSessionManager, times(1)).registerSession(anyLong(), eq(clientHandler));
+        verify(mockSessionManager, times(1)).unregisterSession(anyLong());
+
+        verify(mockSocket, times(1)).close();
+    }
+}
--- socket-app/testing.sh
+++ socket-app/testing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in {1..1000}; do
+for i in {1..100}; do
   username=$(openssl rand -base64 6 | tr -d '/+=' | cut -c 1-8)
   ./send_msg.sh "$username" &
 done
