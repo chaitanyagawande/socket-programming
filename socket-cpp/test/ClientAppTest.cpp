bdb3dd5d75692edd111067ac3bd6aa4cb7e710d2
Atharva Jadhav <30693257+AtharvaJadhav@users.noreply.github.com>
2024-02-19 19:32:39 -0800

    added test files

--- socket-cpp/test/ClientAppTest.cpp
+++ socket-cpp/test/ClientAppTest.cpp
@@ -3,30 +3,28 @@
 #include <chrono>
 #include <thread>
 #include <cstdlib>
-#include <unistd.h>
+// #include <unistd.h>
 #include <cassert>
 
-int main() {
+int main()
+{
     std::cout << "ClientAppTest starting." << std::endl;
 
-
-    int testPort = 12345; 
-
+    int testPort = 12345;
 
     std::string command = "start \"\" \"..\\src\\apps\\ClientApp.cpp\" " + std::to_string(testPort);
 
-    if (system(command.c_str()) == -1) {
+    if (system(command.c_str()) == -1)
+    {
         std::cerr << "Failed to start ClientApp." << std::endl;
         return EXIT_FAILURE;
     }
 
-    
     std::cout << "ClientApp launched, attempting to connect to server on port " << testPort << "." << std::endl;
 
-    
     std::this_thread::sleep_for(std::chrono::seconds(5));
 
-    std::cout << "ClientAppTest completed. Check server logs or other mechanisms to verify connection and communication." << std::endl;
+    std::cout << "ClientAppTest completed" << std::endl;
 
     return EXIT_SUCCESS;
 }
--- socket-cpp/test/ServerAppTest.cpp
+++ socket-cpp/test/ServerAppTest.cpp
@@ -3,27 +3,25 @@
 #include <chrono>
 #include <thread>
 #include <cstdlib>
-#include <unistd.h>
+// #include <unistd.h>
 #include <cassert>
 
-int main() {
+int main()
+{
     std::cout << "ServerAppTest starting." << std::endl;
 
-    const char* command = "start \"\" \"..\\src\\apps\\ServerApp.cpp\"";
+    const char *command = "start \"\" \"..\\src\\apps\\ServerApp.cpp\"";
 
-    if (system(command) == -1) {
+    if (system(command) == -1)
+    {
         std::cerr << "Failed to start ServerApp." << std::endl;
         return EXIT_FAILURE;
     }
 
-
     std::cout << "Assuming the server would start in a real test..." << std::endl;
     std::this_thread::sleep_for(std::chrono::seconds(2));
 
-
     std::cout << "Assuming we send SIGINT to the server in a real test..." << std::endl;
- 
-
 
     std::this_thread::sleep_for(std::chrono::seconds(2));
 
